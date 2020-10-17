414a784f14751b56375bfc1124e25f6766e44625
byt3-m3 <cbaxtertech@gmail.com>
2020-10-17 11:00:58 -0400

    Added new IPv6 validators

--- gcg/schemas/network/core.py
+++ gcg/schemas/network/core.py
@@ -1,13 +1,13 @@
+from gcg.schemas.validators import IPValidator, IPv6Validator, IPv6EUI64Validator, IPv6LinkLocalValidator
 from marshmallow import Schema, fields
-from gcg.schemas.validators import IPValidator
-from marshmallow.validate import Length, Range
+from marshmallow.validate import Range
 
 
 class IPv6Addr(Schema):
-    ipv6_address = fields.Str()
-    eui_64 = fields.Str()
-    link_local = fields.Str()
-    anycast = fields.Str()
+    ipv6_address = fields.Str(validate=IPv6Validator())
+    eui_64 = fields.Str(validate=IPv6EUI64Validator())
+    link_local = fields.Str(validate=IPv6LinkLocalValidator())
+    anycast = fields.Str(validate=IPv6Validator())
 
 
 class IPv4Addr(Schema):
