62c0d18529fe80d2506fabc2e7c53e96d2453f34
byt3-m3 <cbaxtertech@gmail.com>
2020-10-17 11:00:24 -0400

    Added new IPv6Addr validators

--- gcg/schemas/validators.py
+++ gcg/schemas/validators.py
@@ -1,4 +1,5 @@
 import ipaddress
+import re
 
 from marshmallow import validate
 from marshmallow.exceptions import ValidationError
@@ -13,3 +14,37 @@ class IPValidator(validate.Validator):
             ipaddress.ip_address(val)
         except ValueError as error:
             raise ValidationError(str(error))
+
+
+class IPv6Validator(validate.Validator):
+    def __init__(self):
+        pass
+
+    def __call__(self, val):
+        try:
+            ipaddress.ip_address(val)
+
+        except ValueError as error:
+            raise ValidationError(str(error))
+
+
+class IPv6EUI64Validator(validate.Validator):
+    def __init__(self):
+        pass
+
+    def __call__(self, val):
+        pattern = re.compile(".*(/64)", re.I)
+
+        if not pattern.match(val):
+            raise ValidationError("Invalid eui-64 address")
+
+
+class IPv6LinkLocalValidator(validate.Validator):
+    def __init__(self):
+        pass
+
+    def __call__(self, val):
+        pattern = re.compile("(FE80).*", re.I)
+
+        if not pattern.match(val):
+            raise ValidationError("Invalid link-local address")
