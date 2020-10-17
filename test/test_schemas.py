c799b5fcc0247240322d414e84f6bd43b0a2f4b6
byt3-m3 <cbaxtertech@gmail.com>
2020-10-17 09:42:46 -0400

    Added new schema tests

--- test/test_schemas.py
+++ test/test_schemas.py
@@ -5,84 +5,19 @@ from gcg.schemas import (
 )
 
 
-class MyTestCase(unittest.TestCase):
-    def test_schema_base_node_case_1(self):
-        data = {
-            "node_type": "",
-            "hostname": "",
-            "template_type": "",
-            "domain": ""
-        }
-        schema = BaseNode()
-        results = schema.validate(data)
-        self.assertIsInstance(results, dict)
-
-    def test_schema_base_node_case_2(self):
+class IPV4TestCase(unittest.TestCase):
+    def test_case_1(self):
         """
-        Test for BaseNode for missing required parameters
+        Creates a new IPV4Addr() schema
         """
-        data = {
-            "node_type": "",
-
-        }
-        schema = BaseNode()
-        results = schema.validate(data)
-
-        self.assertIsInstance(results, dict)
-        self.assertEqual(len(results), 4)
-
-    def test_schema_base_interface_case_1(self):
-        data = {
-            "link_id": "Gi0/0",
-            "dot1q": "100",
-            "is_mgmt": True,
-            "description": "Test Link",
-            "bandwidth": "100",
-
-        }
-        schema = BaseInterface()
-        results = schema.validate(data)
-        self.assertIsInstance(results, dict)
-
-    def test_schema_base_interface_case_2(self):
-        """
-        Test BaseInteface for missing required parameters
-        """
-        data = {
-            "dot1q": "100",
-            "is_mgmt": True,
-            "description": "Test Link",
-            "bandwidth": "100",
-
-        }
-        schema = BaseInterface()
-        results = schema.validate(data)
-        self.assertIsInstance(results, dict)
-        self.assertEqual(len(results), 1)
-
-    def test_schema_ipv4_addr_case_1(self):
-        data = {
-            "address": "192.168.1.1",
-            "netmask": "255.255.255.0",
-
-        }
-        schema = IPv4Addr()
-        results = schema.validate(data)
-        self.assertIsInstance(results, dict)
-
-    def test_schema_ipv4_addr_case_2(self):
-        """
-        Test IPV4Addr with bad netmask and ipv4 address
-        """
-        data = {
-            "address": "192.168.1.260",
-            "netmask": "255.255.255.260",
-
-        }
         schema = IPv4Addr()
 
-        with self.assertRaises(ValueError):
-            schema.validate(data)
+
+
+        results = schema.validate(data_ipv4_addr_cidr)
+        self.assertIsInstance(results, dict)
+
+
 
 
 if __name__ == '__main__':
--- test/vars.py
+++ test/vars.py
@@ -208,3 +208,13 @@ data_task = {
         }
     ]
 }
+
+data_ipv4_addr_netmask = {
+    "address": "192.168.1.1",
+    "netmask": "255.255.255.0"
+}
+
+data_ipv4_addr_cidr = {
+    "address": "192.168.1.1",
+    "netmask": "255.255.255.0"
+}
