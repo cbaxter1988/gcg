dbee595305b01ea7abd590b062c4cd3477a0edf8
byt3-m3 <cbaxtertech@gmail.com>
2020-10-18 10:49:26 -0400

    Modified the DEBUG env switch to transle strings to boolean types .

--- gcg/env.py
+++ gcg/env.py
@@ -1,11 +1,12 @@
 import os
+from distutils.util import strtobool
 
 APP_PORT = os.getenv("APP_PORT", 5000)
 APP_HOST = os.getenv("APP_HOST", "0.0.0.0")
 DB_HOST = os.getenv("DB_HOST", "192.168.1.182")
 DB_PORT = os.getenv("DB_PORT", 27017)
 DB = os.getenv("DB", "LCG_API")
-DEBUG = os.getenv("DEBUG", True)
+DEBUG = strtobool(os.getenv("DEBUG", "false"))
 
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
